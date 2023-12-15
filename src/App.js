e61a1a30bf39e246271a5af11f5aafbe35586bf7
noonkkot <oyatnunkkot@gmail.com>
2023-12-15 14:37:56 +0900

    App.js import modified

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from 'react';
 import Slider from './components/Slider';
 import Header from './components/Header';
 import HeroSwiper from './components/HeroSwiper';
-import Channel from './components/Channel';
 import ProductYarn from './components/ProductYarn';
 import ProductPackage from './components/ProductPackage';
 import ProductAcc from './components/ProductAcc';
 import ProductBest from './components/ProductBest';
 import Footer from './components/Footer';
-import Kimdaeri from './components/Kimdaeri';
-import YouTubeChannelVideo from './components/YoutubeVideo';
-import YouTubePlayer from './components/YouTubePlayer';
 import './App.css';
 
 
@@ -20,15 +16,11 @@ function App() {
       <Slider />
       <Header />
       <HeroSwiper />
-      {/* <Channel /> */}
-      {/* <YouTubePlayer /> */}
-      {/* <Kimdaeri /> */}
       <ProductYarn />
       <ProductPackage />
       <ProductAcc />
       <ProductBest />
       <Footer />
-      {/* <YouTubeChannelVideo /> */}
     </div>
   );
 }
